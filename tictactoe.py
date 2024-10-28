ff02e0975d3a100c6f9c6ae7de3b3606c2b56068
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-10-28 18:38:39 -0400

    added computer moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 from math import prod
+from random import randint
 
 # Interpret numbers into Xs and Os
 def display_board(board):
@@ -21,22 +22,62 @@ def instructions():
 	print('4\t5\t6\n')
 	print('7\t8\t9\n')
 
+def comp_turn(board):
+	# If X is about to win, stop them
+	trios = get_trios(board)
+	for i, trio in enumerate(trios):
+		if sum(trio) == 4:
+			print('X about to win')
+			# Rows
+			if i < 3:
+				for c in range(len(board[i])):
+					if board[i][c] == 0:
+						board[i][c] = 3
+						return
+			# Cols
+			if i < 6:
+				for r in range(len(board)):
+					if board[r][i-3] == 0:
+						board[r][i-3] = 3
+						return
+			# Major diagonal
+			if i == 6:
+				for j in range(len(trio)):
+					if trio[j] == 0:
+						board[j][j] = 3
+						return
+			# Minor diagonal
+			for j in range(len(trio)):
+				if trio[j] == 0:
+					board[j][2-j] = 3
+					return
+
+	# Otherwise, make random choice
+	r = randint(0, 2)
+	c = randint(0, 2)
+	while board[r][c] != 0:
+		r = randint(0, 2)
+		c = randint(0, 2)
+	board[r][c] = 3
+
 # Process user input - not bothering with choice validation
 def make_turn(board, x_turn):
 	player = 'X' if x_turn else 'O'
-	choice = int(input(f'It\'s {player}\'s turn. Choose your space: '))
-	r = (choice-1) // 3
-	c = (choice-1) % 3
-	board[r][c] = 2 if x_turn else 3
+
+	if x_turn:
+		choice = int(input(f'It\'s {player}\'s turn. Choose your space: '))
+		r = (choice-1) // 3
+		c = (choice-1) % 3
+		while board[r][c] != 0:
+			choice = int(input(f'That space is already taken. Please choose another: '))
+			r = (choice-1) // 3
+			c = (choice-1) % 3
+		board[r][c] = 2
+	else:
+		comp_turn(board)
 	print('\n' * 5)
 
-# Check board for win state. Return:
-# 	0 if nothing
-# 	1 if X wins
-# 	2 if O wins
-# 	(cats game left as default at the end)
-def game_over(board):
-	# Hold all valid trios
+def get_trios(board):
 	trios = []
 
 	# Include all rows
@@ -51,7 +92,18 @@ def game_over(board):
 	trios.append([board[0][0], board[1][1], board[2][2]])
 	trios.append([board[0][2], board[1][1], board[2][0]])
 
-	# Check for X or Y win
+	return trios
+
+# Check board for win state. Return:
+# 	0 if nothing
+# 	1 if X wins
+# 	2 if O wins
+# 	(cats game left as default at the end)
+def game_over(board):
+	# Hold all valid trios
+	trios = get_trios(board)
+
+	# Check for X or O win
 	for trio in trios:
 		if prod(trio) == 8:
 			return 1
