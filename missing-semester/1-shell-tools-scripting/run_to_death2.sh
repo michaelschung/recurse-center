58d7536c8c44c3f13f6face9e87ce37e8972e3de
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-11-19 19:14:01 -0500

    comments

--- missing-semester/1-shell-tools-scripting/run_to_death2.sh
+++ missing-semester/1-shell-tools-scripting/run_to_death2.sh
@@ -1,11 +1,21 @@
+# Very interesting that this way works. I guess that
+# makes sense. Before, I was basically just importing
+# a function so its exit command ended this program.
+# Now, I'm making this script run another script.
+# This also ends up cleaner bc I don't have to
+# separately reroute stdout and stderr.
+
+# Clean up before running again
 rm std*.txt
 
-# Keep count
 count=0
 
 while [[ 1 ]]; do
+    # Increment count
     count=$(($count + 1))
+    # Reroute stdout and stderr to files
     ./routine.sh >> stdout2.txt 2>> stderr2.txt
+    # If previous script failed, print and exit
     if [[ $? -ne 0 ]]; then
         cat stdout2.txt
         cat stderr2.txt
