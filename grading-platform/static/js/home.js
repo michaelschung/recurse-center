dae7b8a838ee8265ea6ba26881a8fc29fda28d27
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-11-25 15:41:16 -0500

    deleting actually works now

--- grading-platform/static/js/home.js
+++ grading-platform/static/js/home.js
@@ -1,41 +1,18 @@
-const deleteForms = document.getElementsByClassName('deleteForm');
-
-for (let i = 0; i < deleteForms.length; i++) {
-    let form = deleteForms[i];
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        let id = event.target.id.slice(-1);
-        console.log(formData);
-        fetch('/process', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: id
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('RESPONSE HANDLER');
-            console.log(data);
-            location.reload();
-        })
-        .catch(error => console.error('Error:', error));
-    });
-}
-
-document.getElementById('dataForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+function deleteGrade(button) {
+    // Get grade_id of row
+    const i = button.parent().parent().children()[2].innerHTML;
     fetch('/process', {
         method: 'POST',
-        body: formData
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            id: i
+        })
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('result').textContent = 'Result: ' + data.result;
+        console.log('RESPONSE HANDLER');
+        console.log(data);
+        location.reload();
     })
     .catch(error => console.error('Error:', error));
-});
+}
