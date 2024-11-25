c2764f1ca77c38c80289be7e1c11858d86373bd3
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-11-25 15:24:54 -0500

    deleting kind of works

--- grading-platform/homepage.py
+++ grading-platform/homepage.py
@@ -3,12 +3,13 @@ TO RUN:
     flask --app homepage run --debug
 """
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import DeclarativeBase
 from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape, Markup
 
-# DB CONFIG
+# DB config
 default = True
-
 DIALECT = 'postgresql+psycopg2'
 USERNAME = 'postgres' if default else 'michaelchung'
 PASSWORD = 'password' if default else 'yay'
@@ -18,32 +19,43 @@ DB_NAME = 'postgres'
 
 # Flask config
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{DIALECT}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}' 
 
-# Uses given connection to execute given query
-def my_query(conn, query):
-    return conn.execute(text(query))
+# SQLAlchemy config
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# Executes text query
+def make_query(query):
+    result = db.session.execute(text(query))
+    db.session.commit()
+    return result
 
 @app.route('/')
-def do_stuff():
-    conn_str = f'{DIALECT}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
-    print(f'Connecting to {conn_str}...')
+def homepage():
+    result = make_query("""
+        SELECT student_name, grade, grade_id
+        FROM grades g
+        JOIN students s
+        ON g.student_id = s.student_id
+    """)
 
-    engine = create_engine(conn_str)
+    return render_template('home.html', results=result.all())
 
-    with engine.connect() as conn:
-        result = my_query(conn, """
-            SELECT student_name, grade, grade_id
-            FROM grades g
-            JOIN students s
-            ON g.student_id = s.student_id
-        """)
-
-        conn.commit()
-
-        return render_template('home.html', results=result.all())
-
-@app.route('/process', methods=['POST'])
+@app.route('/process', methods=['GET', 'POST'])
 def process():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        print('GET')
         print(request.view_args)
+    elif request.method == 'POST':
+        print('POST')
+        id = dict(request.json)['id']
+        print(f'DELETING grade_id {id}')
+        make_query(f"""
+            DELETE FROM grades
+            WHERE grade_id = {id}
+        """)
     return jsonify({'result': 'hi'})
--- grading-platform/static/js/home.js
+++ grading-platform/static/js/home.js
@@ -4,32 +4,38 @@ for (let i = 0; i < deleteForms.length; i++) {
     let form = deleteForms[i];
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        const formData = new FormData(event.target);
         let id = event.target.id.slice(-1);
-        console.log(id);
+        console.log(formData);
         fetch('/process', {
             method: 'POST',
-            body: {'id': id}
-        });
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('RESPONSE HANDLER');
+            console.log(data);
+            location.reload();
+        })
+        .catch(error => console.error('Error:', error));
     });
 }
 
-// document.getElementsByClassName('deleteForm').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     console.log(this);
-// });
-
-// document.getElementById('deleteForm1').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     console.log(this);
-//     const formData = new FormData(event.target);
-//     console.log(formData);
-//     fetch('/process', {
-//         method: 'POST',
-//         body: 'hello'
-//     })
-//     // .then(response => response.json())
-//     // .then(data => {
-//     //     document.getElementById('result').textContent = 'Result: ' + data.result;
-//     // })
-//     // .catch(error => console.error('Error:', error));
-// });
+document.getElementById('dataForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    fetch('/process', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').textContent = 'Result: ' + data.result;
+    })
+    .catch(error => console.error('Error:', error));
+});
