1d2f46176bfa7d1f0c521f154589dfe0500d5b46
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-11-27 14:43:50 -0500

    split new student new grade into two queries

--- grading-platform/homepage.py
+++ grading-platform/homepage.py
@@ -41,20 +41,33 @@ def addGrade():
         name = new_grade.get('inputName')
         grade = new_grade.get('inputGrade')
         make_query(f"""
-            WITH new_student AS (
-                INSERT INTO students (student_name)
-                SELECT '{name}'
-                WHERE NOT EXISTS (
-                    SELECT 1 FROM students WHERE student_name = '{name}'
-                )
-                RETURNING student_id
+            INSERT INTO students (student_name)
+            SELECT '{name}'
+            WHERE NOT EXISTS (
+                SELECT 1 FROM students WHERE student_name = '{name}'
             )
-            INSERT INTO grades (student_id, grade)
-            SELECT
-                COALESCE((SELECT student_id FROM new_student), student_id),
-                {grade}
+        """)
+        make_query(f"""
+            INSERT INTO grades(student_id, grade)
+            SELECT student_id, {grade}
             FROM students
             WHERE student_name = '{name}'
         """)
+        # make_query(f"""
+        #     WITH new_student AS (
+        #         INSERT INTO students (student_name)
+        #         SELECT '{name}'
+        #         WHERE NOT EXISTS (
+        #             SELECT 1 FROM students WHERE student_name = '{name}'
+        #         )
+        #         RETURNING student_id
+        #     )
+        #     INSERT INTO grades (student_id, grade)
+        #     SELECT
+        #         COALESCE((SELECT student_id FROM new_student), student_id),
+        #         {grade}
+        #     FROM students
+        #     WHERE student_name = '{name}'
+        # """)
         return jsonify({'result': 'ADDED'})
     return jsonify({'result': '?'})
