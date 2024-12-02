0c2ba72f8f68e34845036dcbdc57eeba264737a1
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-12-02 16:34:05 -0500

    resolved merge

--- missing-semester/5-version-ctrl/mergepractice/dostuff.py
+++ missing-semester/5-version-ctrl/mergepractice/dostuff.py
@@ -3,30 +3,20 @@ import sys
 def greet():
     print('hi')
 
-<<<<<<< HEAD
 def robot():
     print('h e l l o')
 
-||||||| 4e34ab7
-=======
 def dora():
     print('hola')
 
->>>>>>> dora
 def main():
-<<<<<<< HEAD
     if len(sys.argv) > 1:
-        robot()
+        if sys.argv[1] == 'robot':
+            robot()
+        elif sys.argv[1] == 'dora':
+            dora()
     else:
         greet()
-||||||| 4e34ab7
-    greet()
-=======
-    if len(sys.argv) > 1:
-        dora()
-    else:
-        greet()
->>>>>>> dora
 
 if __name__ == '__main__':
     main()
