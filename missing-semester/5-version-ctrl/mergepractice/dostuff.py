9352a3edfb25f87205dde533612d39e97a2cc0a9
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-12-02 16:23:20 -0500

    dora

--- missing-semester/5-version-ctrl/mergepractice/dostuff.py
+++ missing-semester/5-version-ctrl/mergepractice/dostuff.py
@@ -3,8 +3,14 @@ import sys
 def greet():
     print('hi')
 
+def dora():
+    print('hola')
+
 def main():
-    greet()
+    if len(sys.argv) > 1:
+        dora()
+    else:
+        greet()
 
 if __name__ == '__main__':
     main()
