714643278ddd5767c2ce82c52a9a8766805429b7
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-12-02 16:20:52 -0500

    robot

--- missing-semester/5-version-ctrl/mergepractice/dostuff.py
+++ missing-semester/5-version-ctrl/mergepractice/dostuff.py
@@ -3,8 +3,14 @@ import sys
 def greet():
     print('hi')
 
+def robot():
+    print('h e l l o')
+
 def main():
-    greet()
+    if len(sys.argv) > 1:
+        robot()
+    else:
+        greet()
 
 if __name__ == '__main__':
     main()
