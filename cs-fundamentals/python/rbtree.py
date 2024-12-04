63fa52111c8f3bb8769cd8c059a74621e0801085
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-12-04 14:07:06 -0500

    insert working

--- cs-fundamentals/python/rbtree.py
+++ cs-fundamentals/python/rbtree.py
@@ -9,15 +9,15 @@ Red-black properties:
 """
 
 class Node:
-    def __init__(self, val, is_black):
+    def __init__(self, val, is_black, p=None):
         self.val = val
         self.is_black = is_black      # True = red, False = black
         self.left = None
         self.right = None
-        self.p = None
+        self.p = p
     
     def __repr__(self):
-        return f'Node({self.val})'
+        return f'{self.val}'
     
     def is_leaf(self):
         return self.left is None and self.right is None
@@ -40,18 +40,50 @@ class RBTree:
         self.root = None
         self.size = 0
 
+    def repr_helper(self, root, verbose=False):
+        if root is None:
+            return ''
+        l_str = self.repr_helper(root.left, verbose)
+        r_str = self.repr_helper(root.right, verbose)
+        if verbose:
+            c_str = f'Node(val: {root.val}, color: {'black' if root.is_black else 'red'}, parent: {root.p})\n'
+        else:
+            c_str = f'{root} '
+        return l_str + c_str + r_str
+
+    def __repr__(self):
+        return self.repr_helper(self.root)
+    
+    def verbose(self):
+        return self.repr_helper(self.root, True)
+
     def black_height(self):
         if self.root is None:
             return 0
         return self.root.black_height()
 
-l1 = Node(17, False)
-l1.left = Node(14, True)
-l1.right = Node(21, True)
-r1 = Node(41, True)
-r1.left = Node(30, False)
-r1.right = Node(47, True)
-root = Node(26, True)
-root.left = l1
-root.right = r1
-print(l1.left.black_height())
+    def insert_fixup(self):
+        pass
+
+    def insert_helper(self, root, p, val):
+        if root is None:
+            return Node(val, False, p)
+        if val < root.val:
+            root.left = self.insert_helper(root.left, root, val)
+        else:
+            root.right = self.insert_helper(root.right, root, val)
+        return root
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = Node(val, True)
+        else:
+            self.root = self.insert_helper(self.root, None, val)
+        self.insert_fixup()
+
+tree = RBTree()
+tree.insert(5)
+tree.insert(3)
+tree.insert(8)
+tree.insert(6)
+print(tree.verbose())
